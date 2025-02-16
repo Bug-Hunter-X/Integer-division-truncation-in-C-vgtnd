dd7dec1a0d6661ad2a5ae7ed5c68094010306e7b
BugHunter-X <askzenai@gmail.com>
2025-02-16 15:56:04 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  float x = 10.0; // Use floating point type for accurate division
+  float y = 5.0; // Use floating point type for accurate division
+  float z = x / y; 
+  printf("%.1f\n", z); //Using %.1f to show floating point value
+  return 0;
+}
